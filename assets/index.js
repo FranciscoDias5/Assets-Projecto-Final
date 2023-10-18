56f9e56888760072ee783f1d15db25b816533784
FranciscoDias5 <128630647+FranciscoDias5@users.noreply.github.com>
2023-10-18 17:05:21 +0100

    Add files via upload

---
+++ assets/index.js
@@ -0,0 +1,171 @@
+import quotes from './quotes.svg';
+import send from './Send.svg';
+import shield from './Shield.svg';
+import star from './Star.svg';
+import menu from './menu.svg';
+import close from './close.svg';
+import google from './google.svg';
+import apple from './apple.svg';
+import facebook from './facebook.svg';
+import instagram from './instagram.svg';
+import linkedin from './linkedin.svg';
+import twitter from './twitter.svg';
+import btvlogo from './btvlogo.png';
+import people1 from './people1.jpg';
+import people2 from './people2.jpg';
+import people3 from './people3.jpg';
+import benficacartao1 from './benficacartao1.png';
+import benficalogo5 from './benficalogo5.png';
+import camisolab2 from './camisolab2.png';
+import trubin from './trubin.jpg';
+import samuelsoares from './samuelsoares.jpg';
+import andregomes from './andregomes.jpg';
+import leokokubo from './leokokubo.jpg';
+import antonio from './antonio.jpg';
+import otamendi from './otamendi.jpg';
+import morato from './morato.jpg';
+import tomasaraujo from './tomasaraujo.jpg';
+import juanbernat from './juanbernat.jpg';
+import jurasek from './jurasek.jpg';
+import bah from './bah.jpg';
+import joaovitor from './joaovitor.jpg';
+import florentino from './florentino.jpg';
+import joaoneves from './joaoneves.jpg';
+import kokcu from './kokcu.jpg';
+import joaomario from './joaomario.jpg';
+import aursnes from './aursnes.jpg';
+import chiquinho from './chiquinho.jpg';
+import rafa from './rafa.jpg';
+import gouveia from './gouveia.jpg';
+import dimaria from './dimaria.jpg';
+import neres from './neres.jpg';
+import goncaloguedes from './goncaloguedes.jpg';
+import musa from './musa.jpg';
+import tengstedt from './tengstedt.jpg';
+import cabral from './cabral.jpg';
+import emirates from './emirates.png';
+import adidas from './adidas.png';
+import betano from './betano.png';
+import delta from './delta.png';
+import hospitalluz from './hospitalluz.png';
+import easportsfc from './easportsfc.png';
+import kuantokusta from './kuantokusta.png';
+import leaseplan from './leaseplan.png';
+import monchique from './monchique.png';
+import monlitcabane from './monlitcabane.png';
+import norauto from './norauto.png';
+import nos from './nos.png';
+import quintaparal from './quintaparal.png';
+import repsol from './repsol.png';
+import sagres from './sagres.png';
+import sana from './sana.png';
+import socios from './socios.png';
+import sorare from './sorare.png';
+import suits from './suits.png';
+import wizink from './wizink.png';
+import zumub from './zumub.png';
+import emiratesimg from './emiratesimg.jpg';
+import sagresimg from './sagresimg.jpg';
+import cocacolaimg from './cocacolaimg.jpg';
+import estadionoite from './estadionoite.jpg';
+import simbolobenfica from './simbolobenfica.jpg';
+import eusebio from './eusebio.png';
+import conquistasimportantes from './conquistasimportantes.jpg';
+import velhoestadio from './velhoestadio.jpg';
+import entradaJ from './entradaJ.jpg';
+import benficacampeao from './benficacampeao.jpg';
+import clear from './clear.png';
+import cloud from './cloud.png';
+import drizzle from './drizzle.png';
+import humidity from './humidity.png';
+import rain from './rain.png';
+import search from './search.png';
+import snow from './snow.png';
+import wind from './wind.png';
+
+export {
+	quotes,
+	send,
+	shield,
+	star,
+	menu,
+	close,
+	google,
+	apple,
+	facebook,
+	instagram,
+	linkedin,
+	twitter,
+	btvlogo,
+	people1,
+	people2,
+	people3,
+	benficacartao1,
+	benficalogo5,
+	camisolab2,
+	trubin,
+	samuelsoares,
+	andregomes,
+	leokokubo,
+	antonio,
+	otamendi,
+	morato,
+	tomasaraujo,
+	juanbernat,
+	jurasek,
+	bah,
+	joaovitor,
+	florentino,
+	joaoneves,
+	kokcu,
+	joaomario,
+	aursnes,
+	chiquinho,
+	rafa,
+	gouveia,
+	dimaria,
+	neres,
+	goncaloguedes,
+	musa,
+	tengstedt,
+	cabral,
+	emirates,
+	adidas,
+	betano,
+	delta,
+	hospitalluz,
+	easportsfc,
+	kuantokusta,
+	leaseplan,
+	monchique,
+	monlitcabane,
+	norauto,
+	nos,
+	quintaparal,
+	repsol,
+	sagres,
+	sana,
+	socios,
+	sorare,
+	suits,
+	wizink,
+	zumub,
+	emiratesimg,
+	sagresimg,
+	cocacolaimg,
+	estadionoite,
+	simbolobenfica,
+	eusebio,
+	conquistasimportantes,
+	velhoestadio,
+	entradaJ,
+	benficacampeao,
+	clear,
+	cloud,
+	drizzle,
+	humidity,
+	rain,
+	search,
+	snow,
+	wind,
+};
